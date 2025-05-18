5f5b5b2dab1f2841e48b150ba1492692625c049b
everton-oriente <everton.oriente@hotmail.com>
2025-05-18 22:21:59 +0200

    First Version of Monitor Temperature, Light and Humidity

---
+++ mon_temp_light_hum/target/debug/deps/mon_temp_light_hum.d
@@ -0,0 +1,5 @@
+C:\Everton\Rust_Embassy_Everton\EmbassyRPico\mon_temp_light_hum\target\debug\deps\mon_temp_light_hum.d: src\main.rs
+
+C:\Everton\Rust_Embassy_Everton\EmbassyRPico\mon_temp_light_hum\target\debug\deps\mon_temp_light_hum.exe: src\main.rs
+
+src\main.rs:
---
+++ mon_temp_light_hum/target/debug/mon_temp_light_hum.d
@@ -0,0 +1,1 @@
+C:\Everton\Rust_Embassy_Everton\EmbassyRPico\mon_temp_light_hum\target\debug\mon_temp_light_hum.exe: C:\Everton\Rust_Embassy_Everton\EmbassyRPico\mon_temp_light_hum\src\main.rs
