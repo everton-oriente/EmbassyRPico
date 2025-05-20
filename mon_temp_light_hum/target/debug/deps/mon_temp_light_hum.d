55b00e4f79f17874d64c1d920bd6a9e7c848cac0
everton-oriente <everton.oriente@hotmail.com>
2025-05-20 15:52:27 +0200

    Remove Previous version of monitor temperature, luminosity and humidty

--- mon_temp_light_hum/target/debug/deps/mon_temp_light_hum.d
+++
@@ -1,5 +0,0 @@
-C:\Everton\Rust_Embassy_Everton\EmbassyRPico\mon_temp_light_hum\target\debug\deps\mon_temp_light_hum.d: src\main.rs
-
-C:\Everton\Rust_Embassy_Everton\EmbassyRPico\mon_temp_light_hum\target\debug\deps\mon_temp_light_hum.exe: src\main.rs
-
-src\main.rs:
--- mon_temp_light_hum/target/debug/mon_temp_light_hum.d
+++
@@ -1,1 +0,0 @@
-C:\Everton\Rust_Embassy_Everton\EmbassyRPico\mon_temp_light_hum\target\debug\mon_temp_light_hum.exe: C:\Everton\Rust_Embassy_Everton\EmbassyRPico\mon_temp_light_hum\src\main.rs
